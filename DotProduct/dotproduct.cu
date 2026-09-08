2960b33de2e00c8844a0d7959b1c0445dc412cbe
SumitGarad <sgsuper12373@gmail.com>
2026-09-08 21:44:10 +0530

    removed invalid comparition loop for validating the HOST and Device results

--- DotProduct/dotproduct.cu
+++ DotProduct/dotproduct.cu
@@ -122,12 +122,11 @@ int main(){
     solve_cpu(h_A,h_B,goldenTrace,N); 
 
     // validate the results 
-    bool flag = true; 
-    for( int i = 0 ; i < N ; i++ ){
-        if( fabs(goldenTrace[i] - h_result[i]) > 1e-6){
-            flag = false; 
-            break; 
-        }
+    bool flag;  
+    if( fabs( *h_result - *goldenTrace) >= 1e-5 ){
+        flag = true;
+    }else{
+        flag = false; 
     }
 
     if( flag ){
